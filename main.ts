b8cb023c43d2ed4ad6c9734eca9e11cb33d98dc4
YangQingshan <731495192@qq.com>
2024-04-07 10:52:06 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1665,10 +1665,11 @@ namespace lanqiaoqingshao {
     export function fnReadCm(i2cAddr: number): number {
         let cm: number;
         pins.i2cWriteNumber(i2cAddr, 0X01, NumberFormat.UInt8BE)
-        basic.pause(300)
+        basic.pause(200)
         let readbuf = pins.i2cReadBuffer(i2cAddr, pins.sizeOf(NumberFormat.UInt8LE) * 3)
         cm = (readbuf[0] * 65535 + readbuf[1] * 256 + readbuf[2]) / 1000 / 10;
         cm = Math.round(cm * 100) / 100;
+        basic.pause(200)
         return (cm)
     }
     /**
